267729957a750d8c869b4bef87fc79c50fc2f766
ijwanrizaldia <rizaldipc@gmail.com>
2023-05-31 12:37:29 +0800

    selesai pertemuan 3

---
+++ profile.php
@@ -0,0 +1,55 @@
+<!-- di bagian ini saya sembunyikan -->
+<!-- <?php include "header.php" ?> -->
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/IjwanRizaldiA.jpg" alt="Ijwan Rizaldi Apriansyah" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">2110010088</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Ijwan Rizaldi Apriansyah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Muara Lahei, 02 April 2003</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0821 60586 0065</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">rizaldia@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.jpg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/ig.jpg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/twitter.jpg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/" target="_blank">
+                                <img src="img/medsos/link.jpg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
